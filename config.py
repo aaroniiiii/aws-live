54ae4d61f2e9a60d58566d49ab1133fb0421dafa
aaroniiiii <chngweisoonaaron@gmail.com>
2025-12-06 12:39:28 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cgqrtafng5p5.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee_db"
+custombucket = "aaronchng-bucket"
+customregion = "us-east-1"
+
